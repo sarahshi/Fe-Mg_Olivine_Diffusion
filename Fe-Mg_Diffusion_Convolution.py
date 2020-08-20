71d6c781deb5f22880d843b6e66206401605bf3c
whtowbin <whtowbin@gmail.com>
2020-08-20 14:16:14 -0400

    Best fit and step function implemented

--- Fe-Mg_Diffusion_Convolution.py
+++ Fe-Mg_Diffusion_Convolution.py
@@ -3,6 +3,8 @@ from scipy.optimize import fsolve, root
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import scipy.interpolate as interp
+from numba import jit
 %matplotlib inline
 
 # %%
@@ -51,20 +53,41 @@ def diffusion_kernel(dt, dx):
 
 
 
-def boundary_cond(C=0):
+def boundary_cond(bounds_c):
     # C: single concentration at boundary ( in the future we will support changing boundary conditions. 
     # This can probably be accomplished with a list that pops next value 
-    pad = np.ones(3) * C
+    pad = np.ones(3) * bounds_c
+
+    if len(bounds_c) > 1:
+        pad = (np.ones(3) * bounds_c[0], np.ones(3) * bounds_c[1])
+    
     return pad
 
-def step_condition(X_Intervals, Interval_concentrations, dx):
+def step_condition(X_Intervals, Interval_concentrations, dx ):
+
+    length = abs(np.max(X_Intervals) - np.min(X_Intervals))
+    num_x = int(length/ dx)
+
+
+    segments_x = []
+    segments_c = []
+
     # Define concentration steps. 
+    for idx, interval in enumerate(X_Intervals):
+        interval_num = int((interval[1]-interval[0])/length * num_x)
+        int_x = np.linspace(start = interval[0], stop= interval[1]-dx, num = interval_num) 
+        int_c = np.ones_like(int_x) * Interval_concentrations[idx]
+        segments_x.append(int_x)
+        segments_c.append(int_c)
+    step_x = np.concatenate(segments_x)
+    step_c = np.concatenate(segments_c)
+    return step_x, step_c
 
-    return None
+# %%
 
-# Function to 
+step = step_condition(((0,75),(75,250)), (0.859,0.882), dx =2.5)
 
-
+# %%
 
 
 # TODO this function needs to be written to take an input that is either a Mn or Ni concentration or an XFo vector
@@ -75,19 +98,25 @@ def step_condition(X_Intervals, Interval_concentrations, dx):
 # I think the best way to do this is to have a wrapper function.  That runs the diffusion step function 
 # This wrapper funtion can be a good way to select the different elements. This could easily be written into a GUI
 
-def diffusion_step(vector_c_in, vector_Fo_in, diffusivity_function, diff_kernel_1, der_kernel_2, delta, pad_c, pad_Fo):
+def diffusion_step(vector_c_in, vector_Fo_in, diffusivity_function, diff_kernel_1, der_kernel_2, delta, bounds_c, bounds_Fo):
     """
     Function that takes one step forward for Forsterite dependent diffusion. 
     Parameters:
-
+    bounds_c = tuple of left and right boundary conditions for diffusing species (Fixed bounds at the moment)
+    bounds_c = tuple of left and right boundary conditions for Fo
     Output:
 
     """
     # pad = np.ones(3) * Bound_Concentration put back in
+    # TODO Write function that splits left and right bounds.
+    pad = np.ones(3)
+    pad_c = (bounds_c[0]* pad , bounds_c[1]* pad)
+    pad_Fo = (bounds_Fo[0]* pad , bounds_Fo[1]* pad)
+    # pad generation can probably be taken out of the loop
 
-    vector_c = np.concatenate([pad_c, vector_c_in, pad_c])
+    vector_c = np.concatenate([pad_c[0], vector_c_in, pad_c[1]])
 
-    vector_Fo = np.concatenate([pad_Fo, vector_Fo_in, pad_Fo])
+    vector_Fo = np.concatenate([pad_Fo[0], vector_Fo_in, pad_Fo[1]]) # This might need to step through a larger matrix of values
 
     vector_D = diffusivity_function(vector_Fo)
 
@@ -274,6 +303,33 @@ def D_Func_Ca(T, fO2, alpha, beta, gamma, R= 8.3145):
 
 # %%
 
+# %%
+
+# To fit data I should interpolate the data to the dx spacing. I might be able to use Kringing to get error bars. 
+# Weighted Residuals. Weights will be inverse of STD^2. If STdevs are the same I can use normal functions. 
+ # %%
+
+# Enumerate for loop or iterate index number this allows us to input an array of Fo which we can use to compare. 
+# Pad should also be able to be iterated through time 
+
+def timestepper(vector_c_in, vector_Fo_in, diffusivity_function, bounds_c, timesteps):
+    kernel_1, kernel_2, delta = diffusion_kernel(dt=dt, dx=dx)
+    # At the moment only handles Fo but should diffuse other elements too with a little modification 
+    #results = []
+    results = np.zeros((timesteps, len(vector_c_in)))
+    n = 0
+    for time in range(timesteps):
+        vector_c_in = diffusion_step(vector_c_in = vector_c_in, vector_Fo_in = vector_Fo_in , 
+            diffusivity_function = diffusivity_function, diff_kernel_1 = kernel_1, der_kernel_2 = kernel_2, 
+            delta= delta, bounds_c= bounds_c, bounds_Fo = bounds_c) 
+        vector_Fo_in = vector_c_in
+        results[n]=vector_Fo_in
+        n= n+1
+        #results.append(vector_c_in)
+    #np.array(results)
+    return results #vector_c_in#results
+# %%
+
 fO2 = 1e-7 #2.006191e-05 # Pa
 EFo = 201000. # J/mol
 P = 100000 #200000000. # Pa
@@ -282,19 +338,7 @@ T = 1200 + 273.15 # T in kelvin
 
 D_FO_Func = D_Fo(T=T, P=P, fO2=fO2, alpha = 90, beta = 90, gamma = 0, XFo=None, EFo= 201000, R= 8.3145)
 
-# %%
-D_FO_Func(XFo=np.array((0.9, 0.7, 0.6, 0.77)))
-
-# %%
-
-# To fit data I should interpolate the data to the dx spacing. I might be able to use Kringing to get error bars. 
-# Weighted Residuals. Weights will be inverse of STD^2. If STdevs are the same I can use normal functions. 
-
-# %%
-
-
-
-dx_micron = 5
+dx_micron = 2.5
 dx = dx_micron * 1e-6 # m
 dt =4000 # 100000
 Di = D_FO_Func(0.8)
@@ -303,33 +347,76 @@ CFL = (dt*Di) / (dx**2)
 print(CFL)
 # delta = (dt)/ ((dx) ** 2)
 
-Total_time =  5 * 24 * 60 * 60    # seconds 
-timesteps = 100 #int(Total_time/ dt)
-
- # %%
-pad  = np.ones(3)*0.7
-vector_c_in = np.ones(50)*0.8
+inflect_x = 90
+step_x, step_c = step_condition(((0,inflect_x),(inflect_x,250)), (0.859,0.882), dx_micron)
+bounds_c = (step_c[0],step_c[-1])
+vector_c_in = step_c
 vector_Fo_in = vector_c_in
-D_Fo = D_FO_Func(vector_Fo_in)
-kernel_1, kernel_2, delta = diffusion_kernel(dt=dt, dx=dx)
 
-for time in range(timesteps):
+Total_time =  500 * 24 * 60 * 60    # seconds 
+timesteps = int(Total_time/ dt)
 
-    vector_c_in = diffusion_step(vector_c_in = vector_c_in, vector_Fo_in = vector_Fo_in , 
-        diffusivity_function = D_FO_Func, diff_kernel_1 = kernel_1, der_kernel_2 = kernel_2, 
-        delta= delta, pad_c= pad, pad_Fo = pad) 
-    vector_Fo_in = vector_c_in
-
-vector_c_in
-# %%
+Prof_length = np.max(step_x) # µm
+x_num = Prof_length/dx_micron
 
 num = len(vector_c_in)
 distance =  np.linspace(0, dx_micron*(num), num)
-plt.plot(distance, vector_c_in)
+
+# %%
+Fo_diffusion_results = timestepper(vector_c_in= vector_c_in, vector_Fo_in=vector_Fo_in, diffusivity_function= D_FO_Func, bounds_c=bounds_c, timesteps= timesteps)
+Fo_diffusion_results[-1]
+# %%
+
+
+
+num = len(vector_c_in)
+distance =  np.linspace(0, dx_micron*(num), num)
+plt.plot(distance, Fo_diffusion_results[-1])
 plt.xlabel("Micron")
 plt.ylabel("Fo")
+#plt.ylim(0.65, 0.81)
 
+ol40 = np.loadtxt("/Users/henry/Python Files/Fe-Mg Diffusion/AZ18_WHT06_ol40_C-Prof.txt")
+ol40_Fo = ol40[:,1]
+ol40_x = ol40[:,0]
 
+plt.plot(ol40_x , ol40_Fo)
+plt.plot(step_x,step_c)
+
+# %%
+
+#%%
+def Best_fit(results, data_interp, dt):
+    residual = results-data_interp
+    sum_r2 = np.sum(residual**2, axis = 1)
+    idx_min = np.argmin(sum_r2)
+
+ 
+    sum_r2[idx_min]*1.05 
+
+    time = (idx_min + 1)* dt # seconds
+    time_days = time/(60*60*24)
+    return time, idx_min, sum_r2
+
+# Find a way to return time steps ranges for all data within 5+/-5% of the minimum
+# find roots near to a value then report those ranges. 
+# %%
+# This mentod for estimating time is pretty good but doesnt save enough gradient info. 
+# depending on how I set up MCMC I need to think how the gradient gets inputted.
+
+time_range = np.where((sum_r2 < sum_r2[idx_min]*1.05))
+min_time = (time_range[0].min()+1)*dt /(60*60*24) #days 
+max_time = (time_range[0].max()+1)*dt/(60*60*24) #days 
+#%%
+Fo_interp = interp.interp1d(ol40_x, ol40_Fo)
+data_interp = Fo_interp(distance)
+
+time, idx_min, sum_r2 = Best_fit(Fo_diffusion_results,data_interp, dt)
+
+plt.plot(sum_r2)
+# %%
+plt.hist(sum_r2, bins = 1000)
+plt.ylim(0,300)
 # Sum of the Residuals^2 Write it so that it evaluates it at certain intervals 
 # Maybe divide timesteps into even amounts and then evaluate every N timesteps.
 
@@ -355,11 +442,21 @@ vector_D = np.concatenate([pad_d, d,pad_d])
 
 vector_cxD = vector_D * vector_c
 
+#@jit
+def diffuse(kernel_1,vector_c,timesteps, pad):
+    Diffusion_vec = vector_c
+    for x in range(timesteps):
+        Diffusion_vec = np.convolve(kernel_1,Diffusion_vec, mode="full")[4:-4]#[3:-3]
+        Diffusion_vec = np.concatenate([pad, Diffusion_vec,pad])
+        Diffusion_vec = Diffusion_vec * Diffusion_vec +Diffusion_vec
+    return Diffusion_vec
+#delta = (dt) / ((dx) ** 2)
+#delta*(Diffusion*vector_D)+vector_c
+#%%
 
-Diffusion = np.convolve(kernel_1,vector_c, mode="same")#[3:-3]
-
-delta = (dt) / ((dx) ** 2)
-delta*(Diffusion*vector_D)+vector_c
+%timeit diffuse(kernel_1,vector_c,timesteps, pad)
+#%%
+len(Diffusion)
 
 
 # %%
